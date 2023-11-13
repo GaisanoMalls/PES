5ffc9e71faec81b8a1e41424d7bfaceba786a80b
xen3nxen <joshuaarrogante.x3n@gmail.com>
2023-11-13 17:51:39 +0800

    Refractor Evaluation Form done

--- app/Livewire/EvaluationForm.php
+++ app/Livewire/EvaluationForm.php
@@ -11,8 +11,7 @@ use App\Models\RatingScale;
 use App\Models\Factor;
 use App\Models\FactorRatingScale;
 use App\Models\Part;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Log;
+
 
 class EvaluationForm extends Component
 {
@@ -32,8 +31,14 @@ class EvaluationForm extends Component
     public $date_of_evaluation;
     public $ratingScales;
     public $partsWithFactors;
-    public $selectedValues = [];
+    public $selectedValues = []; // Initialize as an empty array
     public $factorNotes = [];
+
+
+
+    public $selectedRatingScaleIds = [];
+
+    public $selectedEquivalentPoints = [];
     public $recommendationNote = '';
     public $rateesComment = '';
 
@@ -46,14 +51,6 @@ class EvaluationForm extends Component
         $this->employeeId = $employee;
     }
 
-    public function updateSelectedValue($factorId, $value)
-    {
-        $this->selectedValues[$factorId] = $value;
-
-        // Store the updated data in the session.
-        session(['selectedPoints' => $this->selectedValues]);
-    }
-
     public function updateNote($factorId, $note)
     {
         $this->factorNotes[$factorId] = $note;
@@ -61,6 +58,7 @@ class EvaluationForm extends Component
         // Store the updated data in the session.
         session(['factorNotes' => $this->factorNotes]);
     }
+
 
     public function updateComment($commentType)
     {
@@ -74,54 +72,43 @@ class EvaluationForm extends Component
 
     public function calculateTotalRate()
     {
-        $totalRatePart1 = 0;
-        $totalRatePart2 = 0;
-        $totalRatePart3 = 0;
-        $totalRateCombined = 0;
-        // Loop through selectedValues and calculate the total rate for each part separately.
-        foreach ($this->selectedValues as $factorId => $selectedValue) {
-            $factor = Factor::find($factorId); // Assuming you have a Factor model.
-            if ($factor) {
-                if ($factor->part_id === 1) {
-                    $totalRatePart1 += $selectedValue;
-                } elseif ($factor->part_id === 2) {
-                    $totalRatePart2 += $selectedValue;
-                } elseif ($factor->part_id === 3) {
-                    $totalRatePart3 += $selectedValue;
+        $totalRates = [];
+
+        // Loop through each part and calculate the total rate for each part
+        foreach ($this->partsWithFactors as $partWithFactors) {
+            $partTotal = 0;
+
+            // Loop through factors within each part and calculate their total rate
+            foreach ($partWithFactors['factors'] as $factorData) {
+                $factorId = $factorData['factor']->id;
+                $selectedValue = $this->selectedValues[$factorId] ?? 0;
+
+                // Ensure $selectedValue is always an integer
+                if (is_array($selectedValue)) {
+                    $partTotal += array_sum($selectedValue);
+                } else {
+                    $partTotal += (int) $selectedValue; // Convert to integer if not already
                 }
             }
+
+            // Store the total rate for this part
+            $totalRates[$partWithFactors['part']->id] = $partTotal;
         }
 
-        // Calculate the combined total rate for all three parts.
-        $totalRateCombined = $totalRatePart1 + $totalRatePart2 + $totalRatePart3;
-
-        return [
-            'part1' => $totalRatePart1,
-            'part2' => $totalRatePart2,
-            'part3' => $totalRatePart3,
-            'combined' => $totalRateCombined, // Add the combined total rate.
-        ];
+        return $totalRates;
     }
 
 
+    public function updateSelectedValue($factorId, $value)
+    {
+        $this->selectedValues[$factorId] = $value;
+
+        // Store the updated data in the session.
+        session(['selectedPoints' => $this->selectedValues]);
+    }
 
 
     public function submitStep1()
-    {
-        $this->currentStep = 2;
-    }
-    public function submitStep2()
-    {
-        $this->currentStep = 3;
-    }
-    public function submitStep3()
-    {
-        $this->currentStep = 4;
-    }
-
-
-
-    public function submitStep4()
     {
         // Retrieve data from the session
         $recommendationNote = session('recommendationNote', '');
@@ -148,13 +135,14 @@ class EvaluationForm extends Component
         // Get the parts associated with the template
         $parts = Part::where('evaluation_template_id', $this->templateId)->get();
 
-        // Loop through the parts and associated factors to create evaluation point records
+
         foreach ($parts as $part) {
-            $factors = Factor::where('part_id', $part->id)->get();
-            $factorId = 1; // Reset factor ID for each part
+            // Get factors for the specific part and template
+            $factors = Factor::where('evaluation_template_id', $this->templateId)
+                ->where('part_id', $part->id)
+                ->get();
 
             foreach ($factors as $factor) {
-
                 $note = $factorNotes[$factor->id] ?? ''; // Set note to an empty string if it's null
 
                 // Find the appropriate rating scale and factor rating scale based on factor's equivalent points
@@ -163,7 +151,7 @@ class EvaluationForm extends Component
                 $equivalentPoints = $selectedPoints[$factor->id] ?? null;
 
                 foreach ($factorRatingScalesData as $frsData) {
-                    if ($frsData['part_id'] == $part->id && $frsData['factor_id'] == $factorId) {
+                    if ($frsData['part_id'] == $part->id && $frsData['factor_id'] == $factor->id) {
                         // Check if the equivalent points match, and if they do, assign the rating scale and factor rating scale IDs
                         if ($equivalentPoints == $frsData['equivalent_points']) {
                             $ratingScaleId = $frsData['rating_scale_id'];
@@ -174,98 +162,86 @@ class EvaluationForm extends Component
                     }
                 }
 
-                // Create the evaluation point record with the found IDs
+                // After retrieving values for $ratingScaleId and $factorRatingScaleId, add a check:
                 if ($ratingScaleId !== null && $factorRatingScaleId !== null) {
+                    // Create the evaluation point record with the found IDs
                     EvaluationPoint::create([
                         'evaluation_id' => $evaluation->id,
-                        'evaluator_id' => 1002, // Change to the appropriate value
-                        'employee_id' => $this->employeeId, // Change to the appropriate value
+                        'evaluator_id' => 1002,
+                        'employee_id' => $this->employeeId,
                         'evaluation_template_id' => $this->templateId,
-                        'part_id' => $part->id, // Part ID based on the current part
-                        'factor_id' => $factorId, // Factor ID starts from 1 for each part
-                        'rating_scale_id' => $ratingScaleId, // Set the appropriate rating scale ID
-                        'factor_rating_scale_id' => $factorRatingScaleId, // Set the appropriate factor rating scale ID
-                        'points' => $equivalentPoints, // Get points based on factor ID
-                        'note' => $note, // Set the note with a default value if it's null
+                        'part_id' => $part->id,
+                        'factor_id' => $factor->id,
+                        'rating_scale_id' => $ratingScaleId,
+                        'factor_rating_scale_id' => $factorRatingScaleId,
+                        'points' => $equivalentPoints,
+                        'note' => $note,
                     ]);
+                } else {
+                    // Handle the case when rating_scale_id or factor_rating_scale_id is null
+                    // This might involve setting default values or skipping the insertion
+                    // Or generating an error message for investigation
                 }
-
-                $factorId++; // Increment factor ID for the next factor
             }
-
-            // Reset factor ID for the next part
-            $factorId = 1;
         }
         session()->forget(['recommendationNote', 'rateesComment', 'selectedPoints', 'factorNotes']);
         return redirect()->route('evaluations.index');
     }
 
 
-    public function goBack()
-    {
-        if ($this->currentStep > 1) {
-            $this->currentStep--;
-        }
-
-        // Re-initialize the component if going back to step 1
-        if ($this->currentStep === 1) {
-        }
-    }
-
     public function render()
     {
-        // Load employee data (modify this to load your employee data)
-        // $this->employee = Employee::find($this->employee_id);
         $employee = Employee::find($this->employeeId);
-
         $department = Department::find($employee->department_id);
         $this->departmentName = $department->name;
         $this->employeeIdCompany = $employee->employee_id;
         $this->name = $employee->first_name . ' ' . $employee->last_name;
         $this->position =  $employee->position;
         $this->date_hired = $employee->date_hired;
-        // Load rating scales
+
         $this->ratingScales = RatingScale::all();
-        // dd($department);
-        // Fetch parts associated with the template ID
+
         $parts = Part::where('evaluation_template_id', $this->templateId)->get();
         $this->partsWithFactors = [];
 
         foreach ($parts as $part) {
             $factors = Factor::where('part_id', $part->id)->get();
-            $factorCounter = 1;
+            $factorsData = [];
 
             foreach ($factors as $factor) {
-                $factor->rating_scales = FactorRatingScale::where([
-                    'evaluation_template_id' => $this->templateId,
-                    'part_id' => $part->id,
-                    'factor_id' => $factorCounter,
-                ])->get();
-                foreach ($factor->rating_scales as $ratingScale) {
-                    $ratingScale->acronym = RatingScale::find($ratingScale->rating_scale_id)->acronym;
-                }
-                $factorCounter++;
+                $factorData = [
+                    'factor' => $factor,
+                    'rating_scales' => FactorRatingScale::where([
+                        'evaluation_template_id' => $this->templateId,
+                        'part_id' => $part->id,
+                        'factor_id' => $factor->id,
+                    ])->get()->map(function ($scale) {
+                        $ratingScale = RatingScale::find($scale->rating_scale_id);
+                        $scale->acronym = $ratingScale->acronym;
+                        return $scale;
+                    }),
+                ];
+
+                $factorsData[] = $factorData;
             }
 
             $this->partsWithFactors[] = [
                 'part' => $part,
-                'factors' => $factors,
+                'factors' => $factorsData,
             ];
         }
-        $totalRates = $this->calculateTotalRate();
 
+
+        $totalRates = $this->calculateTotalRate();
 
         return view('livewire.evaluation-form', [
             'employee' => $this->employee,
             'employeeId' => $this->employeeId,
             'department' => $this->departmentName,
             'ratingScales' => $this->ratingScales,
-            'partsWithFactors' => $this->partsWithFactors,
             'currentStep' => $this->currentStep,
-            'totalRatePart1' => $totalRates['part1'],
-            'totalRatePart2' => $totalRates['part2'],
-            'totalRatePart3' => $totalRates['part3'],
-            'totalRateCombine' => $totalRates['combined']
+            'totalRates' => $totalRates,
+
         ]);
     }
 }
